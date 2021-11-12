3ec4231c06375492af2e5c88fcb24a4c35e8940e
atulbansalofficial <atulbansal9@gmail.com>
2021-11-13 01:42:20 +0530

    todo react App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Todo App</h2>
+        <div className="container">
+          <div>
+            <input type="text" placeholder="Enter todo list Heare" />
+            <button className="addBtn">Add</button>
+          </div>
+          <div>
+            <ul>
+              <li>Dummy list</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
